81187e14475ada14210fb35245c7de5eceb01cb0
ohyeah1783 <kawaishinta@gmail.com>
2019-08-25 12:44:28 +0900

    Initial Commit

---
+++ SlideshowPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowPractice
+//
+//  Created by Shintaro Kawai on 2019/08/25.
+//  Copyright © 2019 shintaro.kawai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
